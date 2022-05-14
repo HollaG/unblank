dd4cc7b5c9cf4b1e1e2b70ca38ba4dac3b7f8315
marcu <marcussoh38@gmail.com>
2022-05-14 12:18:42 +0800

    added system color mode

--- src/theme.ts
+++ src/theme.ts
@@ -1,8 +1,9 @@
 import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
 
 const config: ThemeConfig = {
-    initialColorMode: "light",
+    initialColorMode: "system",
     useSystemColorMode: false,
+    
 };
 
 const theme = extendTheme({
