566183dd89839d12c3f4eb0c71d7d237e259b2e1
marcu <marcussoh38@gmail.com>
2022-06-21 22:18:25 +0800

    updated service worker

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,22 @@
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+        navigator.serviceWorker.register("sw.js").then(
+            function (registration) {
+                // Registration was successful
+                console.log(
+                    "ServiceWorker registration successful with scope: ",
+                    registration.scope
+                );
+            },
+            function (err) {
+                // registration failed :(
+                console.log("ServiceWorker registration failed: ", err);
+            }
+        );
+    });
+}
 
-
-let CACHE_NAME = "unblank-cache-1";
+let CACHE_NAME = "unblank-cache-2";
 const urlsToCache = ["/", "/words.json", "/words_dictionary.json"];
 self.addEventListener("install", function (event) {
     // Perform install steps
@@ -13,23 +29,31 @@ self.addEventListener("install", function (event) {
 });
 
 self.addEventListener("fetch", function (event) {
+    if (!event.request.url.match(/http/i)) return;
     event.respondWith(
-        caches.match(event.request).then(function (response) {
-            if (response) {
-                return response;
-            }
-            return fetch(event.request);
+        caches.match(event.request).then((cacheRes) => {
+            return (
+                cacheRes ||
+                fetch(event.request).then((fetchRes) => {
+                    return caches.open(CACHE_NAME).then((cache) => {
+                        cache.put(event.request.url, fetchRes.clone());
+                        return fetchRes;
+                    });
+                })
+            );
         })
     );
 });
 
-self.addEventListener("install", function (event) {
-    // Perform install steps
+self.addEventListener("activate", (event) => {
+    // Delete old versions of the cache
     event.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-            console.log("Opened cache");
-            return cache.addAll(urlsToCache);
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys
+                    .filter((key) => key !== CACHE_NAME)
+                    .map((key) => caches.delete(key))
+            );
         })
     );
-    self.skipWaiting();
 });
